74f7eba0c5a0470bcd0de6e953e4f2a72ce1c879
Mutti u rehman <mutti.mughal@gmail.com>
2023-01-03 01:50:54 +0500

    learning(sequelize | association): set sequelize association on user and products and update function accordingly

--- app.js
+++ app.js
@@ -8,6 +8,9 @@ const sequelize = require('./util/database.js');
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
+const Product = require('./models/product');
+const User = require('./models/user');
+
 const app = express();
 
 app.set('view engine', 'ejs');
@@ -16,15 +19,50 @@ app.set('views', 'views');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
+/**
+ * Setting user available for all routes 
+ */
+app.use((req, res, next) => {
+    User
+    .findByPk(1)
+    .then(user => {
+        req.user = user;
+        next();
+    })
+    .catch(err => {
+        console.log("Error caused at middleware on getting user", err);
+    });
+});
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
 app.use(errorController.get404);
 
-sequelize.sync()
-.then((res) => {
-    app.listen(3000);
-})
-.catch(err => {
-    console.log(err);
-});
+/**
+ * setting Association here regarding table in DB
+ * when we sync, it will create references in between tables
+ */
+Product.belongsTo(User, { constraints: true, onDelete: 'CASCADE'});
+User.hasMany(Product);
+
+/**
+ * this will create tables if not exsit with all associations as per models
+ * also this will create User with default value if not exit any
+ */
+sequelize
+    .sync()  // .sync({ force: true}) if we want overwrite
+    .then((result) => {
+        return User.findByPk(1);
+    })
+    .then(user => {
+        if(!user) {
+            return User.create({name: "Mutti u rehman", email: 'mutti.dev@gmail.com'});
+        }
+        return user;
+    })
+    .then(user => {
+        app.listen(3000);
+    })
+    .catch(err => {
+        console.log(err);
+    });
--- controllers/admin.js
+++ controllers/admin.js
@@ -14,7 +14,18 @@ exports.postAddProduct = (req, res, next) => {
   const price = req.body.price;
   const description = req.body.description;
 
-  Product.create({
+  /**
+   * This is function sequelize provide when we set associations
+   * it replica of this funciton 
+   * Product.create({
+   *   title: title,
+   *   price: price,
+   *   imageUrl: imageUrl,
+   *   description: description,
+   *   userId: req.user.id
+   *  })
+   */
+  req.user.createProduct({
     title: title,
     price: price,
     imageUrl: imageUrl,
@@ -29,16 +40,17 @@ exports.postAddProduct = (req, res, next) => {
 exports.getEditProduct = (req, res, next) => {
   const editMode = req.query.edit;
   const prodId = req.params.productId;
-  Product.findByPk(prodId)
-  .then((product) => {
-    if (!product) {
+  req.user.getProducts({ where: { id: prodId}})
+  // Product.findByPk(prodId)
+  .then((products) => {
+    if (!products) {
       return res.redirect('/');
     }
     res.render('admin/edit-product', {
       pageTitle: 'Edit Product',
       path: '/admin/edit-product',
       editing: editMode,
-      product: product
+      product: products[0]
     });
   })
   .catch((err) => {
@@ -73,7 +85,7 @@ exports.postEditProduct = (req, res, next) => {
 };
 
 exports.getProducts = (req, res, next) => {
-  Product.findAll()
+  req.user.getProducts()
   .then(products => {
     res.render('admin/products', {
       prods: products,

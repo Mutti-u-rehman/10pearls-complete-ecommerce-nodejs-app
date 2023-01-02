69b2a32811097fd2973e2b2682b3f29a603eed03
Mutti u rehman <mutti.mughal@gmail.com>
2023-01-02 14:14:29 +0500

    learning: database connection is set with sequelize

--- util/database.js
+++ util/database.js
@@ -1,10 +1,13 @@
-const mysql = require('mysql2');
+const Sequelize = require('sequelize');
 
-const pool = mysql.createPool({
-    host: 'localhost',
-    user: 'root',
-    database: 'node-complete',
-    password: 'Mughal1234'
-});
+const sequelize = new Sequelize(
+    'node-complete',
+    'root',
+    'Mughal1234',
+    { 
+        dialect: 'mysql',
+        host: 'localhost'
+    }
+);
 
-module.exports = pool.promise();
+module.exports = sequelize;

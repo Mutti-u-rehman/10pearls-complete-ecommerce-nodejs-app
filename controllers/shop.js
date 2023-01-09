c06f93be4ca6e4c1b6381169048ac8846b7124cf
Mutti u rehman <mutti.mughal@gmail.com>
2023-01-09 23:46:42 +0500

    learning(cart): deleting cart items

--- controllers/shop.js
+++ controllers/shop.js
@@ -89,8 +89,7 @@ exports.postCart = (req, res, next) => {
         through: { quantity: newQuantity}
       });
     })
-    .then((product) => {
-      console.log(product);
+    .then(() => {
       res.redirect('/cart');
     })
     .catch(err => {
@@ -100,10 +99,19 @@ exports.postCart = (req, res, next) => {
 
 exports.postCartDeleteProduct = (req, res, next) => {
   const prodId = req.body.productId;
-  Product.findById(prodId, product => {
-    Cart.deleteProduct(prodId, product.price);
-    res.redirect('/cart');
-  });
+  req.user
+    .getCart()
+    .then(cart => {
+      return cart.getProducts({where: {id: prodId}});
+    })
+    .then(products => {
+      const product = products[0];
+      return product.cartItem.destroy();
+    })
+    .then(result => {
+      res.redirect('/cart');
+    })
+    .catch(err => console.log(err));
 };
 
 exports.getOrders = (req, res, next) => {

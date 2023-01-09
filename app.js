7f9d8674513243c68476e3b0c314be69ceb1e88f
Mutti u rehman <mutti.mughal@gmail.com>
2023-01-09 23:58:37 +0500

    Learning(association): create association for Order, User and Product using OrderItem

--- app.js
+++ app.js
@@ -12,6 +12,8 @@ const Product = require('./models/product');
 const User = require('./models/user');
 const Cart = require('./models/cart');
 const CartItem = require('./models/cart-item');
+const Order = require('./models/order');
+const OrderItem = require('./models/order-item');
 
 const app = express();
 
@@ -61,6 +63,11 @@ Cart.belongsTo(User);
  */
 Cart.belongsToMany(Product, { through: CartItem});
 Product.belongsToMany(Cart, { through: CartItem});
+
+Order.belongsTo(User);
+User.hasMany(Order);
+
+Order.belongsTo(Product, {through: OrderItem})
 
 /**
  * this will create tables if not exsit with all associations as per models

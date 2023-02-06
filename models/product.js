5e9bb743ce8ebac2dff1bb33ef6c4201bc5c2bf9
Mutti u rehman <mutti.mughal@gmail.com>
2023-02-07 00:58:53 +0500

    feat(core): setup product methods to fetch all and single data

--- models/product.js
+++ models/product.js
@@ -1,5 +1,9 @@
-const { resolve } = require('path');
-
+/**
+ * we import ObjectId from mongodb and If we have to multiple date type
+ * of BSON then we can import complete mongodb and then we can access each
+ * type in from one object
+ */ 
+const { ObjectId } = require('mongodb');
 const getDb = require('../util/database').getDb;
 
 class Product {
@@ -23,14 +27,38 @@ class Product {
       });
   }
 
-   static async fetchAll() {
+  /**
+   * Get all product from Database
+   * @returns Products
+   */
+  static async fetchAll() {
+  const _db = getDb();
+  return _db
+    .collection('products')
+    .find()
+    .toArray()
+    .then(products => {
+      console.log("Fetch All products");
+      return products;
+    })
+    .catch(err => {
+      console.log("Error on fetch all products: ",err);
+    });
+  }
+
+  /**
+   * Fetch a single product based on Id
+   * @param {ObjectId} prodId 
+   * @returns Single Product
+   */
+  static async findById(prodId) {
     const _db = getDb();
     return _db
       .collection('products')
-      .find()
-      .toArray()
+      .find({_id: new ObjectId(prodId)})
+      .next()
       .then(products => {
-        console.log("Fetch All products");
+        console.log("Fetch One product Based on Id");
         return products;
       })
       .catch(err => {

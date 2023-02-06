0f35ffb02745fc177fc3e47e5710fbf4ced2603c
Mutti u rehman <mutti.mughal@gmail.com>
2023-02-06 22:56:17 +0500

    refactor: improve readability of the code

--- util/database.js
+++ util/database.js
@@ -1,17 +1,20 @@
-const mongodb = require('mongodb');
+const mongodb = require("mongodb");
 const MongoClient = mongodb.MongoClient;
 const connectionString = process.env.ATLAS_URI;
 
 let _db_client;
 
-const mongoConnect = callback => {
-  MongoClient.connect(connectionString, { useNewUrlParser: true, useUnifiedTopology: true })
-    .then(client => {
-      console.log('Connected!');
+const mongoConnect = (callback) => {
+  MongoClient.connect(connectionString, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+    .then((client) => {
+      console.log("Connected!");
       _db_client = client;
       callback();
     })
-    .catch(err => {
+    .catch((err) => {
       console.log(err);
       throw err;
     });
@@ -21,7 +24,7 @@ const getDb = () => {
   if (_db_client) {
     return _db_client;
   }
-  throw 'No database found!';
+  throw "No database found!";
 };
 
 exports.mongoConnect = mongoConnect;

47ae4062a8f5fd13f551f5afd273aa5d0549375c
Mutti u rehman <mutti.mughal@gmail.com>
2023-02-07 00:57:38 +0500

    refactor(clear): clear dead-code

--- controllers/shop.js
+++ controllers/shop.js
@@ -16,17 +16,9 @@ exports.getProducts = (req, res, next) => {
 
 exports.getProduct = (req, res, next) => {
   const prodId = req.params.productId;
-  // Product.findAll({ where: { id: prodId } })
-  //   .then(products => {
-  //     res.render('shop/product-detail', {
-  //       product: products[0],
-  //       pageTitle: products[0].title,
-  //       path: '/products'
-  //     });
-  //   })
-  //   .catch(err => console.log(err));
   Product.findById(prodId)
     .then(product => {
+      console.log(product);
       res.render('shop/product-detail', {
         product: product,
         pageTitle: product.title,

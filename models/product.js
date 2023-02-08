09097c29b8cbad8cd9f40906a1dc29f8a928ce28
Mutti u rehman <mutti.mughal@gmail.com>
2023-02-08 23:52:56 +0500

    feat(core): user id set for each product

--- models/product.js
+++ models/product.js
@@ -7,12 +7,13 @@ const { ObjectId } = require('mongodb');
 const getDb = require('../util/database').getDb;
 
 class Product {
-  constructor(title, price, description, imageUrl, id) {
+  constructor(title, price, description, imageUrl, id, userId) {
     this.title = title;
     this.price = price;
     this.description = description;
     this.imageUrl = imageUrl;
     this._id = id ? new ObjectId(id) : null;
+    this.userId = userId
   }
 
   /**

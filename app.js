e6e8ed6ea951072077fc426e30edfaea0c7e7d85
Mutti u rehman <mutti.mughal@gmail.com>
2023-02-09 23:53:32 +0500

    new user Object set up in routes

--- app.js
+++ app.js
@@ -31,7 +31,7 @@ app.use((req, res, next) => {
     User
     .findById('63e3ed18467c7bfce44c0cac')
     .then(user => {
-        req.user = user;
+        req.user = new User(user.name, user.email, user.cart, user._id);
         next();
     })
     .catch(err => {

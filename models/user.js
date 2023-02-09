22fa121580d856eae11692226a3cd875ada5cab3
Mutti u rehman <mutti.mughal@gmail.com>
2023-02-09 20:09:14 +0500

    feat(core): add to cart method added

--- models/user.js
+++ models/user.js
@@ -3,29 +3,54 @@ const mongodb = require('mongodb');
 
 const ObjectId = mongodb.ObjectId;
 class User {
-    constructor(_username, _email) {
-        this.username = _username;
-        this.email = _email;
+  constructor(_username, _email, _cart) {
+    this.username = _username;
+    this.email = _email;
+    this.cart = _cart;
+  }
+
+  save() {
+    const _db = getDb();
+    return _db.collection("users").insertOne(this);
+  }
+
+  addToCart(product) {
+    const cartProduct = this.cart.items.findIndex(
+      (cp) => cp._id === product._id
+    );
+
+    if (cartProduct === -1) {
+      const updateCart = {
+        items: [{ ...product, quantity: 1 }],
+      };
     }
 
-    save() {
-        const _db = getDb();
-        return _db.collection('users').insertOne(this);
-    }
+    this.updateCart(this._id, updatedCart);
+  }
 
-    static async findById(userId) {
-        const _db = getDb();
-        return _db
-        .collection('users')
-        .findOne({_id: new ObjectId(userId)})
-        .then(user => {
-            console.log(user);
-            return user;
-        })
-        .catch(err => {
-            console.log(err);
-        });
-    }
+  static async updateCart(id, updatedCart) {
+    const _db = getDb();
+    _db
+      .collection("users")
+      .updateOne(
+        { _id: new ObjectId(id) }, 
+        { $set: { cart: updatedCart }}
+        );
+  }
+
+  static async findById(userId) {
+    const _db = getDb();
+    return _db
+      .collection("users")
+      .findOne({ _id: new ObjectId(userId) })
+      .then((user) => {
+        console.log(user);
+        return user;
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
 }
 
 module.exports = User;

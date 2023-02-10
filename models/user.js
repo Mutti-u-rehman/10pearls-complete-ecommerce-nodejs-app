c730b41dd8b881d65eafb2b4bef9b0e15d275054
Mutti u rehman <mutti.mughal@gmail.com>
2023-02-10 23:40:25 +0500

    update product id for all reference files

--- models/user.js
+++ models/user.js
@@ -30,7 +30,7 @@ class User {
     }
 
     const updatedCart = {
-      items: [{ ...product, quantity: 1 }],
+      items: [{ productId: new ObjectId(product._id), quantity: 1 }],
     };
     const _db = getDb();
     _db

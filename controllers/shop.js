31341fb36097de0ce18020ec1ebd70213d75f9bf
Mutti u rehman <mutti.mughal@gmail.com>
2023-02-11 01:04:26 +0500

    update getCart  render method as new getCart method added on User Model

--- controllers/shop.js
+++ controllers/shop.js
@@ -45,17 +45,13 @@ exports.getIndex = (req, res, next) => {
 exports.getCart = (req, res, next) => {
   req.user
     .getCart()
-    .then(cart => {
-      return cart
-        .getProducts()
-        .then(products => {
-          res.render('shop/cart', {
-            path: '/cart',
-            pageTitle: 'Your Cart',
-            products: products
-          });
-        })
-        .catch(err => console.log(err));
+    .then(products => { 
+      res.render('shop/cart', {
+        path: '/cart',
+        pageTitle: 'Your Cart',
+        products: products
+      });
+        
     })
     .catch(err => console.log(err));
 };

50023fcc4805befa8f3a4e837fad482122428ba7
Mutti u rehman <mutti.mughal@gmail.com>
2023-02-12 12:57:54 +0500

    setup route for deleting cart items

--- routes/shop.js
+++ routes/shop.js
@@ -12,9 +12,7 @@ router.get('/products/:productId', shopController.getProduct);
 
 router.get('/cart', shopController.getCart);
 router.post('/cart', shopController.postCart);
-
-
-// router.post('/cart-delete-item', shopController.postCartDeleteProduct);
+router.post('/cart-delete-item', shopController.postCartDeleteProduct);
 
 // router.post('/create-order', shopController.postOrder);
 

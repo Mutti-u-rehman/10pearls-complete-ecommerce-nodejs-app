f30425ae1d67ed3910ed6cfc91cea4a346730e36
Mutti u rehman <mutti.mughal@gmail.com>
2023-02-13 00:20:51 +0500

    enable route for get order again

--- routes/shop.js
+++ routes/shop.js
@@ -14,10 +14,10 @@ router.get('/cart', shopController.getCart);
 router.post('/cart', shopController.postCart);
 router.post('/cart-delete-item', shopController.postCartDeleteProduct);
 
+router.get('/orders', shopController.getOrders);
 router.post('/create-order', shopController.postOrder);
 
-// router.get('/orders', shopController.getOrders);
-
 // router.get('/checkout', shopController.getCheckout);
+
 
 module.exports = router;

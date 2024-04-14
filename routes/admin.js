87dad38321634873e6ed7344ea4a8b15eeeebeb2
Mutti u rehman <mutti.rehman@adib.com>
2024-04-14 10:32:32 +0400

    feature(core) add mongoose and fix basic breaking changes

--- routes/admin.js
+++ routes/admin.js
@@ -4,11 +4,11 @@ const router = express.Router();
 const adminController = require('../controllers/admin');
 
 
-// router.get('/products', adminController.getProducts);
+router.get('/products', adminController.getProducts);
 // router.get('/add-product', adminController.getAddProduct);
 // router.get('/edit-product/:productId', adminController.getEditProduct);
 
-// router.post('/add-product', adminController.postAddProduct);
+router.post('/add-product', adminController.postAddProduct);
 // router.post('/edit-product', adminController.postEditProduct);
 // router.post('/delete-product', adminController.postDeleteProduct);
 
